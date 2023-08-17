42a7042195225dedfd8fc4173a52bc1284484333
Gustavo Souza <iamgustavosouza@gmail.com>
2023-08-17 01:40:46 -0300

    feat: initial configs

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@libs': './src/libs',
+          }
+        },
+      ],
+    ],
   };
 };
